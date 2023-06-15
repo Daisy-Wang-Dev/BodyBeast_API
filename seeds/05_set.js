c82c33fab7e20f9de269aeaffc71d086c652e487
Xueying Wang <daiszws19@gmail.com>
2023-06-15 14:27:13 +0100

    import seed data for set table

--- seeds/05_set.js
+++ seeds/05_set.js
@@ -1,10 +1,11 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+const setData = require("../seed-data/set");
+
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('set').del()
-  await knex('set').insert(
-);
+  await knex("set").del();
+  await knex("set").insert(setData);
 };
