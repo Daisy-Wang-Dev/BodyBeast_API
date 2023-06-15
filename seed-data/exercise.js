6617854db6416df01dc9cc3c0cc736bc6201cf23
Xueying Wang <daiszws19@gmail.com>
2023-06-15 14:25:09 +0100

    Add seed file for set table and modify the seed file in exercise table

--- seed-data/exercise.js
+++ seed-data/exercise.js
@@ -1,9 +1,7 @@
 module.exports = [
-  { id: 1, name: "Leg Curl" },
-  { id: 2, name: "Squats" },
-  { id: 3, name: "Leg Press" },
-  { id: 4, name: "Bicep Curl" },
-  { id: 5, name: "Lat Pull Down" },
-  { id: 6, name: "Shoulder press" },
+  { id: 1, name: "Bicep Curl" },
+  { id: 2, name: "Lat Pull Down" },
+  { id: 3, name: "Leg Curl" },
+  { id: 4, name: "Leg Press" },
 
 ];
---
+++ seed-data/set.js
@@ -0,0 +1,15 @@
+module.exports = [
+  { id: 1, weight: 10, rep: 12, exericse_routine_id: 1 },
+  { id: 2, weight: 10, rep: 12, exericse_routine_id: 1 },
+  { id: 3, weight: 10, rep: 12, exericse_routine_id: 1 },
+  { id: 4, weight: 23, rep: 12, exericse_routine_id: 1 },
+  { id: 5, weight: 23, rep: 12, exericse_routine_id: 1 },
+  { id: 6, weight: 25, rep: 10, exericse_routine_id: 1 },
+
+  { id: 7, weight: 18, rep: 12, exericse_routine_id: 2 },
+  { id: 8, weight: 23, rep: 10, exericse_routine_id: 2 },
+  { id: 9, weight: 23, rep: 10, exericse_routine_id: 2 },
+  { id: 10, weight: 50, rep: 12, exericse_routine_id: 2 },
+  { id: 11, weight: 80, rep: 10, exericse_routine_id: 2 },
+  { id: 12, weight: 100, rep: 8, exericse_routine_id: 2 },
+];
