268cc6845d86fc4e6535226a26fff5fe154ec137
Xueying Wang <daiszws19@gmail.com>
2023-06-17 16:22:52 +0100

    Run seed files on all table

--- migrations/20230614132412_create_user_table.js
+++ migrations/20230614132412_create_user_table.js
@@ -8,7 +8,7 @@ exports.up = function (knex) {
     table.string("name", 255).notNullable();
     table.string("user_name", 270).notNullable();
     table.string("email").notNullable();
-    table.string("password").notNullable();
+    // table.string("password").notNullable();remove from the table
     table.date("date_of_birth").notNullable(); //YYYY-MM-DD
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table
